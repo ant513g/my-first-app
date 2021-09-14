fd66a0443e4a0e4fdcd7d7b57475d4db149e9f30
ant513g <89939545+ant513g@users.noreply.github.com>
2021-09-13 23:22:33 -0400

    Added missing link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 
 function App() {
-  return ( <
-    div className = "App" >
+  return ( < div className = "App" >
     <
     header className = "App-header" >
     <
     p > Hello CSCI E - 39! < /p> <
-    p >
-    My name is Ana! <
-    /p> < /
-    header > <
+    p > My name is Ana! < /p>
+
+    <
+    a className = "App-link"
+    href = "https://reactjs.org"
+    target = "_blank"
+    rel = "noopener noreferrer" >
+    Learn React < /a> <
+    /header > <
     /div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,10 @@ import {
   screen
 } from '@testing-library/react';
 import App from './App';
-import '@testing-library/jest-dom'
+
 
 test('renders learn react link', () => {
-  render( < App />);
+  render( < App / > );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
